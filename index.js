cea624d42d8fe7980d3d259b702a977f8c82d04d
Sijan Dahal <47341534+sijandahal@users.noreply.github.com>
2021-01-26 01:39:37 +0545

    added svg path fill on hover

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-var countDownTimer = new Date ('March 26, 2021 1:14:60').getTime();
+var countDownTimer = new Date('March 26, 2021 1:14:60').getTime();
 
 function myCountDown() {
-  setInterval(function() {
-   
-  var currentDate = new Date().getTime();
-  var timeLeft = countDownTimer - currentDate;
+  setInterval(function () {
 
-  var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
-  document.querySelector(".days").innerHTML = days;
-  document.querySelector(".hours").innerHTML = hours ;
-  document.querySelector(".minutes").innerHTML = minutes; 
-  document.querySelector(".seconds").innerHTML = seconds;
+    var currentDate = new Date().getTime();
+    var timeLeft = countDownTimer - currentDate;
 
-if (timeLeft < 0) {
-  clearInterval(myfunc);
-  document.querySelector(".days").innerHTML = days;
-  document.querySelector(".hours").innerHTML = hours ;
-  document.querySelector(".minutes").innerHTML = minutes; 
-  document.querySelector(".seconds").innerHTML = seconds;
-  alert ("Time's Up");
-}
+    var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    document.querySelector(".days").innerHTML = days;
+    document.querySelector(".hours").innerHTML = hours;
+    document.querySelector(".minutes").innerHTML = minutes;
+    document.querySelector(".seconds").innerHTML = seconds;
 
-  },1000)
+    if (timeLeft < 0) {
+      clearInterval(myfunc);
+      document.querySelector(".days").innerHTML = days;
+      document.querySelector(".hours").innerHTML = hours;
+      document.querySelector(".minutes").innerHTML = minutes;
+      document.querySelector(".seconds").innerHTML = seconds;
+      alert("Time's Up");
+    }
+
+  }, 1000)
 }
 
 myCountDown();
